4d997ada1fdf6e6439b2436d9caa64388e7c3f0c
Wen Hui Teh <wenhui.teh23@gmail.com>
2020-04-25 05:01:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,12 @@
 require_relative './test'
 AVERAGE_HEIGHT_IN_INCHES = 67
 
+name = "alexis"
+age = "20"
+favorite_food = "soup"
+height_in_inches = "54"
+
+
 # Your Code Here!
 
 ## Do not modify below this line!
