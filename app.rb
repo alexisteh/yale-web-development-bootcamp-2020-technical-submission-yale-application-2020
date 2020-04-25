86a93650fb48275470745f4bca2d1c3a8cd65536
Wen Hui Teh <wenhui.teh23@gmail.com>
2020-04-25 05:03:57 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ AVERAGE_HEIGHT_IN_INCHES = 67
 name = "alexis"
 age = "20"
 favorite_food = "soup"
-height_in_inches = "59"
+height_in_inches = 59
 height_in_inches > AVERAGE_HEIGHT_IN_INCHES ? height_message =  "You are taller than average." : height_message = "You are not taller than average."
 
 
